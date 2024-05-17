287bda725973a9be0fe5915d2bf8ccafa0423d8c
h.yaghooti <hamedyaghooti1992@gmail.com>
2024-05-18 01:21:59 +0330

    add useFetch

---
+++ src/hooks/useFetch.ts
@@ -0,0 +1,22 @@
+import { useState, useEffect } from "react";
+
+export const useFetch = (url: string) => {
+  const [data, setData] = useState(null);
+  const [error, setError] = useState(null);
+  const [loading, setLoading] = useState(true);
+  useEffect(() => {
+    const fetchData = async () => {
+      try {
+        const response = await fetch(url);
+        const json = await response.json();
+        setData(json);
+      } catch (error) {
+        setError(error);
+      } finally {
+        setLoading(false);
+      }
+    };
+    fetchData();
+  }, [url]);
+  return { data, error, loading };
+};
