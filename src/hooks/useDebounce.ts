12f8000a22f29e5492fc220897f044b9572b06ee
h.yaghooti <hamedyaghooti1992@gmail.com>
2024-05-18 02:01:30 +0330

    modify useDebounce hook

--- src/hooks/useDebounce.ts
+++ src/hooks/useDebounce.ts
@@ -1,6 +1,6 @@
 import { useState, useEffect } from "react";
 
-export const useDebounce = (value: string, delay: number) => {
+export function useDebounce(value: string, delay: number) {
   const [debouncedValue, setDebouncedValue] = useState(value);
   useEffect(() => {
     const timer = setTimeout(() => {
@@ -11,4 +11,4 @@ export const useDebounce = (value: string, delay: number) => {
     };
   }, [value, delay]);
   return debouncedValue;
-};
+}
