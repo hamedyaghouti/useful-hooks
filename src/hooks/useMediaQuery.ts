5daa9101eca9abb534171da7687dd5468b4f5934
h.yaghooti <hamedyaghooti1992@gmail.com>
2024-05-18 02:03:24 +0330

    modify useMediaQuery

--- src/hooks/useMediaQuery.ts
+++ src/hooks/useMediaQuery.ts
@@ -1,18 +1,19 @@
 import { useState, useEffect } from "react";
 
-export const useMediaQuery = (
+export function useMediaQuery(
   query: `(max-width: ${number}px)` | `(min-width: ${number}px)`
-) => {
+) {
   const [matches, setMatches] = useState(
     () => window.matchMedia(query).matches
   );
   useEffect(() => {
     const mediaQuery = window.matchMedia(query);
-    const handleChange = (event: MediaQueryListEvent) => setMatches(event.matches);
+    const handleChange = (event: MediaQueryListEvent) =>
+      setMatches(event.matches);
     mediaQuery.addEventListener("change", handleChange);
     return () => {
       mediaQuery.removeEventListener("change", handleChange);
     };
   }, [query]);
   return matches;
-};
+}
