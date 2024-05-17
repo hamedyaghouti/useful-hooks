a751a23333aa2e341a40f031c6bcf762e81c756a
h.yaghooti <hamedyaghooti1992@gmail.com>
2024-05-18 02:04:17 +0330

    add return type of useToggle

--- src/hooks/useToggle.ts
+++ src/hooks/useToggle.ts
@@ -1,9 +1,9 @@
-import { useState } from 'react';
+import { useState } from "react";
 
-export const useToggle = (initialValue: boolean | undefined = false) => {
+export function useToggle(initialValue: boolean | undefined = false) {
   const [value, setValue] = useState<boolean>(initialValue);
   const toggle = () => {
     setValue((prevValue) => !prevValue);
   };
-  return [value, toggle];
-};
+  return [value, toggle] as [boolean, () => void];
+}
